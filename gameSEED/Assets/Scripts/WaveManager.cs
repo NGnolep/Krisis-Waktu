5c64f7be29e314a5e1e5405142a4bff404f7094b
Daappoy <m.alghifaridaffa@gmail.com>
2024-09-11 12:56:46 +0700

    modified WaveManager Script, ntar ku lanjutin lagi

--- gameSEED/Assets/Scripts/WaveManager.cs
+++ gameSEED/Assets/Scripts/WaveManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+
+public class Wave
+{
+    public GameObject enemyPrefab;
+    public int enemyCount;
+    public float SpawnRate;
+}
 
 public class WaveManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Wave[] waves;
+    private int currentWaveIndex = 0;
+    private bool isSpawning = false;
+    private float timeBetweenWaves = 5f;
+    private float countdown = 2f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(!isSpawning)
+        {
+            if(countdown <= 0f)
+            {
+                
+            }
+        }
     }
+
 }
