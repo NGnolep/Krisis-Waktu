9db611ebf7818332b7a93e2225a4735f6199a708
Daappoy <m.alghifaridaffa@gmail.com>
2024-09-13 18:30:36 +0700

    all menu pages done

--- gameSEED/Assets/Scripts/shopScript.cs
+++ gameSEED/Assets/Scripts/shopScript.cs
@@ -5,6 +5,10 @@ using TMPro;
 
 public class shopScript : MonoBehaviour
 {
+    [Header("Script References")]
+    public EnemySpawn enemySpawn;
+
+    [Header("Object References")]
     public TextMeshProUGUI Gold;
     public GameObject shopPanel;
     public GameObject HPButton;
@@ -22,6 +26,10 @@ public class shopScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(enemySpawn.waveValue == 0)
+        {
+            shopPanel.SetActive(true);
+            
+        }
     }
 }
